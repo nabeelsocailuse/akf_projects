2cd3c31a125d0c6878f7c117e5dcfdf08767f5ed
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-01-28 13:34:46 +0000

    stake holder matrix & risk register updated by Mubashir

--- akf_projects/akf_projects/doctype/risk_register/risk_register.js
+++ akf_projects/akf_projects/doctype/risk_register/risk_register.js
@@ -1,8 +1,24 @@
-// Copyright (c) 2024, Nabeel Saleem and contributors
-// For license information, please see license.txt
+// Developer Mubashir Bashir
 
 // frappe.ui.form.on("Risk Register", {
-// 	refresh(frm) {
+// 	// refresh(frm) {
 
-// 	},
+// 	// },
+//     severity: function (frm){
+//         calculate_rating(frm);               
+//     },
+//     likelihood: function (frm){
+//         calculate_rating(frm);        
+//     },
 // });
+
+// function calculate_rating(frm){
+//     if (frm.doc.severity && frm.doc.likelihood) {
+//         let rating = frm.doc.severity * frm.doc.likelihood;
+//         frm.set_value('rating', rating);
+//         // frm.save() 
+//     }
+//     else {
+//         frm.set_value('rating', '');
+//     } 
+// }

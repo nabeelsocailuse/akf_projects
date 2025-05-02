06ae504de07b186c871acba2057bfa3f35e8796a
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-05-02 06:47:09 +0000

    financial stats updated.

--- akf_projects/customizations/overrides/project/financial_stats.py
+++ akf_projects/customizations/overrides/project/financial_stats.py
@@ -1,35 +1,36 @@
 import frappe, ast
 
-""" 
+"""
 Nabeel Saleem
 Date: 23-12-2024
 """
 @frappe.whitelist()
 def get_transactions(filters=None):
-	if(type(filters) == str): filters = ast.literal_eval(filters) 
-	
+	if(type(filters) == str): filters = ast.literal_eval(filters)
+
 	args = {}
 	args.update(get_donation(filters))
 	args.update(get_funds_transfer(filters))
 	args.update(get_purchasing(filters))
-	
+
 	# total_allocation = (donation + received_pledge + received_funds)
-	total_allocation = args['donation'] + args['received_pledge'] +  args['received_funds']
+	# total_allocation = args['donation'] + args['received_pledge'] +  args['received_funds']
+	total_allocation = args['donation']
 	total_pledge = args['pending_pledge']
 	transfered_funds = args['transfered_funds']
 	received_funds = args['received_funds']
 	total_purchase = args['purchased_amount']
 	# balance_amount = (total_allocation + received_funds -( transfered_funds + total_purchase ))
-	balance_amount = (total_allocation -( transfered_funds + total_purchase ))
-	
+	# balance_amount = (total_allocation -( transfered_funds + total_purchase ))
+	balance_amount = get_remaining_balance(filters)
 	filters.update({
 		"total_allocation": total_allocation,
 		"total_purchase": total_purchase,
 		"balance_amount": balance_amount,
 	})
-	
+
 	set_project_allocation(filters)
-	
+
 	return{
 		"total_allocation": total_allocation,
 		"total_pledge": total_pledge,
@@ -40,7 +41,7 @@ def get_transactions(filters=None):
 	}
 
 def set_project_allocation(filters):
-	frappe.db.sql(""" Update `tabProject` 
+	frappe.db.sql(""" Update `tabProject`
 				Set custom_total_allocation = %(total_allocation)s,
 					custom_total_purchase = %(total_purchase)s,
 					custom_remaining_allocation = %(balance_amount)s
@@ -49,7 +50,36 @@ def set_project_allocation(filters):
 				""",filters )
 
 def get_donation(filters):
-	def get_conditions():
+	def get_conditions_donations():
+		conditions = " and company = %(company)s " if(filters.get('company')) else ""
+		conditions += " and cost_center = %(cost_center)s " if(filters.get('cost_center')) else ""
+		conditions += " and service_area = %(service_area)s " if(filters.get('service_area')) else ""
+		conditions += " and subservice_area = %(subservice_area)s " if(filters.get('subservice_area')) else ""
+		conditions += " and product = %(product)s " if(filters.get('product')) else ""
+		conditions += " and project = %(project)s " if(filters.get('project')) else ""
+		conditions += " and donor = %(donor)s " if(filters.get('donor')) else ""
+		conditions += " and account = %(account)s " if(filters.get('account')) else ""
+		return conditions
+
+	donation = frappe.db.sql(f"""
+ 		Select
+			sum(credit-debit) as balance
+		From
+  			`tabGL Entry` gl
+		Where
+  			docstatus=1
+			and is_cancelled=0
+			and account in (select name from tabAccount where account_type="Equity")
+			{get_conditions_donations()}
+		-- Group By
+			-- cost_center, account, donor
+		-- Having
+			-- balance>0
+		-- Order By
+			-- balance desc
+	""", filters, as_dict=0)
+	# frappe.throw(f"{donation}")
+	def get_conditions_pledge():
 		conditions = " and d.company = %(company)s " if(filters.get('company')) else ""
 		conditions += " and p.cost_center = %(cost_center)s " if(filters.get('cost_center')) else ""
 		conditions += " and p.pay_service_area = %(service_area)s " if(filters.get('service_area')) else ""
@@ -59,31 +89,31 @@ def get_donation(filters):
 		conditions += " and p.donor = %(donor)s " if(filters.get('donor')) else ""
 		conditions += " and p.equity_account = %(account)s " if(filters.get('account')) else ""
 		return conditions
-	
-	data = frappe.db.sql(f""" 
-	Select 
-		ifnull(sum(case when (d.contribution_type='Donation' and d.status!='Return') then p.net_amount else 0 end),0) as donation_received,
-		ifnull(sum(case when d.status='Return' then p.net_amount else 0 end),0) as donation_returned,
-		ifnull(sum(case when d.contribution_type='Pledge' then (p.net_amount-p.outstanding_amount) else 0 end),0) as received_pledge,
-		ifnull(sum(case when d.contribution_type='Pledge' then p.outstanding_amount else 0 end),0) as pending_pledge
-	From
-		`tabDonation` d
-		INNER JOIN  
-		`tabPayment Detail` p
-		ON (d.name=p.parent)
-	Where
-		d.docstatus=1
-		{get_conditions()}
-		""", filters, as_dict=1)
-	
+
+	pledge = frappe.db.sql(f"""
+		Select
+			-- ifnull(sum(case when (d.contribution_type='Donation' and d.status!='Return') then p.net_amount else 0 end),0) as donation_received,
+			-- ifnull(sum(case when d.status='Return' then p.net_amount else 0 end),0) as donation_returned,
+			ifnull(sum(case when d.contribution_type='Pledge' then (p.net_amount-p.outstanding_amount) else 0 end),0) as received_pledge,
+			ifnull(sum(case when d.contribution_type='Pledge' then p.outstanding_amount else 0 end),0) as pending_pledge
+		From
+			`tabDonation` d
+			INNER JOIN
+			`tabPayment Detail` p
+			ON (d.name=p.parent)
+		Where
+			d.docstatus=1
+			{get_conditions_pledge()}
+	""", filters, as_dict=1)
+
 	args = {
-		"donation": 0.0, 
+		"donation": donation[0][0] or 0.0,
 		"received_pledge": 0.0,
 		"pending_pledge": 0.0,
 	}
-	for d in data:
+	for d in pledge:
 		args.update({
-			"donation": (d.donation_received - d.donation_returned), 
+			# "donation": (d.donation_received - d.donation_returned),
 			"received_pledge": d.received_pledge,
 			"pending_pledge": d.pending_pledge,
 		})
@@ -101,17 +131,17 @@ def get_funds_transfer(filters):
 			conditions += " and ff_donor = %(donor)s " if(filters.get('donor')) else ""
 			conditions += " and ff_account = %(account)s " if(filters.get('account')) else ""
 			return conditions
-			
-		return frappe.db.sql(f""" 
-		Select 
+
+		return frappe.db.sql(f"""
+		Select
 			sum(ff_transfer_amount) as transfer_amount
 		From
-			`tabFunds Transfer From` 
+			`tabFunds Transfer From`
 		Where
 			docstatus=1
 			{get_conditions()}
 		""", filters)[0][0] or 0.0
-		
+
 	def get_received_funds():
 		def get_conditions():
 			conditions = " and ft_company = %(company)s " if(filters.get('company')) else ""
@@ -123,9 +153,9 @@ def get_funds_transfer(filters):
 			conditions += " and ft_donor = %(donor)s " if(filters.get('donor')) else ""
 			conditions += " and ft_account = %(account)s " if(filters.get('account')) else ""
 			return conditions
-		
-		return frappe.db.sql(f""" 
-		Select 
+
+		return frappe.db.sql(f"""
+		Select
 			sum(ft_amount) as transfer_amount
 		From
 			`tabFunds Transfer To`
@@ -152,11 +182,11 @@ def get_purchasing(filters):
 		return conditions
 	return {
 		"purchased_amount": frappe.db.sql(f"""
-			Select 
+			Select
 				sum(debit) total_consumed
-			From 
+			From
 				`tabGL Entry` gl
-			Where 
+			Where
 				is_cancelled=0
 				and ifnull(party, '')!=''
 				and voucher_type  in ('Purchase Invoice')
@@ -164,3 +194,25 @@ def get_purchasing(filters):
 				{get_conditions()}
 			""", filters)[0][0] or 0.0
 	}
+
+def get_remaining_balance(filters):
+	def get_conditions():
+		conditions = " and company = %(company)s " if(filters.get('company')) else ""
+		conditions += " and cost_center = %(cost_center)s " if(filters.get('cost_center')) else ""
+		conditions += " and service_area = %(service_area)s " if(filters.get('service_area')) else ""
+		conditions += " and subservice_area = %(subservice_area)s " if(filters.get('subservice_area')) else ""
+		conditions += " and product = %(product)s " if(filters.get('product')) else ""
+		conditions += " and project = %(project)s " if(filters.get('project')) else ""
+		conditions += " and donor = %(donor)s " if(filters.get('donor')) else ""
+		conditions += " and account = %(account)s " if(filters.get('account')) else ""
+		return conditions
+	return frappe.db.sql(f""" Select
+			sum(credit-debit) as balance
+		From
+  			`tabGL Entry` gl
+		Where
+  			docstatus=1
+			and is_cancelled=0
+			and account in (select name from tabAccount where account_type="Equity")
+			{get_conditions()}
+	""", filters)[0][0] or 0.0

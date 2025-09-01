68eb2c6424327103e35440deceaee794c5eaf939
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-09-01 11:46:22 +0000

    Project approval form changes

--- akf_projects/akf_projects/doctype/project_approval_form/project_approval_form.js
+++ akf_projects/akf_projects/doctype/project_approval_form/project_approval_form.js
@@ -2,120 +2,74 @@
 
 frappe.ui.form.on("Project Approval Form", {
     refresh: function(frm) {
-        if (!frm.is_new() && frm.doc.docstatus == 1 && (frappe.user.has_role("Program Manager P&D") || frappe.user.has_role("Administrator"))) {
-            let btn = frm.add_custom_button("Create Project", function() {
-                frappe.route_options = {
-                    project_name: frm.doc.project_title,
-                    custom_region: frm.doc.region,
-                    custom_district: frm.doc.district,
-                    custom_tehsil: frm.doc.tehsil,
-                    custom_uc: frm.doc.uc,
-                    custom_latitude: frm.doc.latitude,
-                    custom_longitude: frm.doc.longitude,
-                    estimated_costing: frm.doc.estimated_project_budget,
-                    custom_project_approval_form: frm.doc.name,
-                    project_type: frm.doc.project_type, 
-                    fund_class: frm.doc.fund_class, 
-                };
-                frappe.set_route("Form", "Project", "new-project");
+        if (!frm.is_new() && frm.doc.docstatus == 1) {
+            frappe.db.get_list("ToDo", {
+                filters: {
+                    reference_type: frm.doctype,
+                    reference_name: frm.doc.name,
+                    allocated_to: frappe.session.user
+                },
+                fields: ["name"]
+            }).then(r => {
+                if (r.length > 0 && (frappe.user.has_role("Program Manager P&D") || frappe.user.has_role("Administrator"))) {
+                    let btn = frm.add_custom_button("Create Project", function() {
+                        frappe.route_options = {
+                            project_name: frm.doc.project_title,
+                            custom_region: frm.doc.region,
+                            custom_district: frm.doc.district,
+                            custom_tehsil: frm.doc.tehsil,
+                            custom_uc: frm.doc.uc,
+                            custom_latitude: frm.doc.latitude,
+                            custom_longitude: frm.doc.longitude,
+                            estimated_costing: frm.doc.estimated_project_budget,
+                            custom_project_approval_form: frm.doc.name,
+                            project_type: frm.doc.project_type,
+                            fund_class: frm.doc.fund_class,
+                        };
+                        frappe.set_route("Form", "Project", "new-project");
+                    });
+                    btn.addClass("btn-primary");
+                }
             });
-            btn.addClass("btn-primary");
         }
-        /////////////////////////////
-        // frm.trigger("showWorkFlowState");
-		set_filters(frm);
-		set_fund_class_permissions(frm);
+
+        empty_fields(frm);
+        set_filters(frm);
+        set_fund_class_permissions(frm);
+        block_fields(frm);
+        render_state_table(frm);
     },
     onload: function(frm) {
-        if (frm.is_new()) {
-            frm.set_value('next_approver_id', '');
-            frm.set_value('next_approver_name', '');
-            frm.set_value('custom_state_data', '');
-            frm.set_value('custom_state_html', '');
-        }
+        empty_fields(frm);
 		set_filters(frm);
 		set_fund_class_permissions(frm);
+        render_state_table(frm);
     },
 	region: function(frm) {
-        // clearing dependent fields
         frm.set_value("district", "");
         frm.set_value("tehsil", "");
         set_filters(frm);
     },
     district: function(frm) {
-        // clearing tehsil when district changes
         frm.set_value("tehsil", "");
         set_filters(frm);
-    },
-
-    showWorkFlowState: function(frm){
-		if(frm.doc.custom_state_data==undefined) {
-			frm.set_df_property('custom_state_html', 'options', '<p></p>')
-		}else{
-			const stateObj = JSON.parse(frm.doc.custom_state_data)
-			
-			const desiredOrder = [
-				"Requested by Program Manager",
-				"Recommended by Chairman",
-				"Rejected by Chairman",
-				"Returned by Chairman",
-				"Recommended by Director Program",
-				"Rejected by Director Program",
-				"Returned by Director Program",
-				"Recommended by General Manager P&D",
-				"Rejected by General Manager P&D",
-				"Returned by General Manager P&D",
-				"Recommended by Director P&D",
-				"Rejected by Director P&D",
-				"Returned by Director P&D",
-				"Recommended by Chairman P&D",
-				"Returned by Chairman P&D",
-				"Recommended to CEO",
-				"Approved by Chief Executive Officer",
-				"Returned by Chief Executive Officer",
-			];
-
-			const orderedStates = desiredOrder
-				.filter(state => stateObj.hasOwnProperty(state)) 
-				.map(state => ({ key: state, ...stateObj[state] })); 
-			
-			let rows = ``;
-			let idx = 1
-			for (const data of orderedStates) {
-				const dt = moment(data.modified_on).format("DD-MM-YYYY hh:mm:ss a");
-
-				rows += `
-				<tr>
-					<th scope="row">${idx}</th>	
-					<td scope="row">${data.current_state}</td>
-					<td scope="row">${data.current_user}</td>
-					<td scope="row">${data.department}</td>
-					<td class="">${dt}</td>
-				</tr>`;
-				idx += 1;
-			}
-			let _html_ = `
-			<table class="table">
-				<thead class="thead-dark">
-					<tr>
-					<th scope="col">#</th>
-					<th class="text-left" scope="col">Approval State</th>
-					<th class="text-left" scope="col">User</th>
-					<th class="text-left" scope="col">Department</th>
-					<th scope="col">DateTime</th>
-					</tr>
-				</thead>
-				<tbody>
-					${rows}
-				</tbody>
-			</table>`;
-			frm.set_df_property('custom_state_html', 'options', _html_)
-		}
-	}
-
-	
+    }	
 });
 
+
+function empty_fields(frm) {
+    if (frm.is_new()) {
+        frm.set_value('next_approver_id', '');
+        frm.set_value('next_approver_name', '');
+        frm.set_value('custom_state_data', '');
+        frm.set_value('custom_state_html', '');
+
+        frm.set_df_property("custom_state_html", "hidden", 1);
+    }
+    else {
+        frm.set_df_property("custom_state_html", "hidden", 0); 
+    }
+}
 
 function set_filters(frm) {
     // Filter District by Region
@@ -138,11 +92,97 @@ function set_filters(frm) {
 }
 
 function set_fund_class_permissions(frm) {
+    console.log("checking fund class permissions");
+    console.log(frappe.user.roles);
+    
+    
     if (frappe.user.has_role("Fund Class Officer")) {
+        console.log("fund class officer - making fund class editable and required");
+        
         frm.set_df_property("fund_class", "reqd", 1);
         frm.set_df_property("fund_class", "read_only", 0);
     } else {
         frm.set_df_property("fund_class", "reqd", 0);
         frm.set_df_property("fund_class", "read_only", 1);
     }
+}
+
+function block_fields(frm) {
+    // states where Program Manager should be able to edit
+    const editable_states = [
+        "Draft",
+        "Returned by Chairman",
+        "Returned by Director Program",
+        "Returned by CEO"
+    ];
+
+    if (editable_states.includes(frm.doc.workflow_state) 
+        && frappe.user_roles.includes("Program Manager")) {
+        console.log("making fields editable for Program Manager");
+            
+
+        // make all fields editable
+        Object.keys(frm.fields_dict).forEach(f => {
+            frm.set_df_property(f, "read_only", 0);
+        });
+
+    } else {
+        // lock all fields except workflow_state
+        Object.keys(frm.fields_dict).forEach(f => {
+            if (f !== "workflow_state" && f !== "fund_class") {
+                frm.set_df_property(f, "read_only", 1);
+            }
+        });
+    }
+}
+
+function render_state_table(frm) {
+    if (!frm.doc.custom_state_data) return;
+
+    let rows = "";
+    let data_list = [];
+    try {
+        data_list = JSON.parse(frm.doc.custom_state_data);
+    } catch(e) {
+        console.log("Invalid JSON in custom_state_data");
+        return;
+    }
+
+    let idx = 1;
+    data_list.forEach(data => {
+        let dt = frappe.datetime.str_to_user(data.datetime);
+        let next_info = "";
+
+        if (data.next_user || data.next_role) {
+            let user_name = data.next_user || "";
+            let role = data.next_role || "";
+            next_info = `${frappe.utils.escape_html(user_name)} (${frappe.utils.escape_html(role)})`;
+        }
+
+        rows += `
+            <tr>
+                <th scope="row">${idx}</th>
+                <td>${frappe.utils.escape_html(data.employee_name || "")}</td>
+                <td>${frappe.utils.escape_html(data.current_state || "")}</td>
+                <td>${dt}</td>
+                <td>${next_info}</td>
+            </tr>`;
+        idx++;
+    });
+
+    let html = `
+    <table class="table table-bordered table-sm">
+        <thead class="thead-dark">
+            <tr>
+                <th>#</th>
+                <th>Employee Name</th>
+                <th>Current State</th>
+                <th>DateTime</th>
+                <th>Next State</th>
+            </tr>
+        </thead>
+        <tbody>${rows}</tbody>
+    </table>`;
+
+    frm.set_df_property("custom_state_html", "options", html);
 }
--- akf_projects/akf_projects/doctype/project_approval_form/project_approval_form.py
+++ akf_projects/akf_projects/doctype/project_approval_form/project_approval_form.py
@@ -1,51 +1,120 @@
 # Mubashir Bashir 19-04-2025
 
 import frappe
-from frappe.utils import get_datetime, get_fullname
+import json
+from frappe.utils import now, get_fullname
 from frappe.model.document import Document
 
 class ProjectApprovalForm(Document):
-	def validate(self):
-		# if self.is_new():
-		# 	self.posting_date = frappe.utils.nowdate()		
-		self.record_application_state()
+    def validate(self):
+        pass
 
-	# def on_submit(self):
-	# 	self.notify_program_managers_pd()
-	
-	def record_application_state(self):
-		if(hasattr(self, 'workflow_state')):
-			state_dict = eval(self.custom_state_data) if(self.custom_state_data) else {}
-			# if(self.workflow_state not in state_dict):
-			state_dict.update({f"{self.workflow_state}": {
-				"current_user": f"<b>{get_fullname(frappe.session.user)}</b>",
-				"current_state": f"<b>{self.workflow_state}</b>",
-				# "department": f"<b>{self.get_department() or '-'}</b>",
-				"modified_on": get_datetime(),
-				"next_state": f"<b>{self.workflow_state}</b>",
-			}})
-			self.custom_state_data =  frappe.as_json(state_dict)
-	
-	def get_department(self):
-		return frappe.get_value("Employee", {'user_id': frappe.session.user}, ['department'])
+    def before_save(self):
+        # Enforce Fund Class Officer must enter fund_class before proceeding
+        if self.workflow_state == "Fund Class Assigned by Finance" and not self.fund_class:
+            frappe.throw("Fund Class must be filled before assigning fund class.")
 
-	def notify_program_managers_pd(self):
-		users = frappe.get_all("Has Role", 
-			filters={"role": "Program Manager P&D"},
-			fields=["parent"]
-		)
+        # 1. Detect workflow state change
+        old_state = None
+        if self.name:  # existing doc
+            old_state = frappe.db.get_value(self.doctype, self.name, "workflow_state")
+        new_state = self.workflow_state
 
-		for user in users:
-			user_id = user.parent
+        if old_state != new_state:
+            # ---- custom workflow tracking logic ----
 
-			notification = frappe.new_doc("Notification Log")
-			notification.update({
-				"subject": "New Project Approval Submitted",
-				"email_content": f"A Project Approval Form has been submitted: {self.name}",
-				"for_user": user_id,
-				"document_type": self.doctype,
-				"document_name": self.name,
-				"type": "Alert"
-			})
-			notification.insert(ignore_permissions=True)
-			frappe.msgprint(f'Notification sent to {user.parent}')
+            # find the active workflow for this doctype
+            wf = frappe.get_doc("Workflow", {"document_type": self.doctype, "is_active": 1})
+            # find matching transition (from current state + next_state)
+            transition = next(
+                (t for t in wf.transitions if t.state == old_state and t.next_state == new_state),
+                None
+            )
+            if not transition:
+                return
+
+            next_state = transition.next_state
+
+            # find allowed role for next state
+            next_state_doc = next((s for s in wf.states if s.state == next_state), None)
+            allowed_role = next_state_doc.allow_edit if next_state_doc else None
+
+            next_user = None
+            if allowed_role:
+                # all users with this role
+                users = frappe.get_all("Has Role", filters={"role": allowed_role}, fields=["parent"])
+                user_ids = [u.parent for u in users]
+
+                if user_ids:
+                    # check if any of these users have a User Permission for this service_area
+                    matching_users = frappe.get_all(
+                        "User Permission",
+                        filters={
+                            "allow": "Service Area",
+                            "for_value": self.service_area,
+                            "user": ["in", user_ids]
+                        },
+                        fields=["user"]
+                    )
+
+                    if matching_users:
+                        # pick first matching user
+                        next_user = matching_users[0].user
+                    else:
+                        # fallback to first role-holder
+                        next_user = user_ids[0]
+
+            # update next_approver fields
+            if next_user:
+                self.next_approver_id = next_user
+                self.next_approver_name = get_fullname(next_user)
+
+
+            # update next_approver_id
+            if next_user:
+                self.next_approver_id = next_user
+                self.next_approver_name = get_fullname(next_user)
+
+            # append transition details to custom_state_data
+            history = []
+            if self.custom_state_data:
+                try:
+                    history = json.loads(self.custom_state_data)
+                except Exception:
+                    history = []
+
+            history.append({
+                "employee_name": get_fullname(frappe.session.user),
+                "previous_state": old_state,
+                "current_state": new_state,
+                "datetime": now(),
+                "next_user": get_fullname(next_user) if next_user else None,
+                "next_role": allowed_role
+            })
+
+            self.custom_state_data = json.dumps(history, indent=2)
+
+
+	# def notify_program_managers_pd(self):
+	# 	users = frappe.get_all("Has Role", 
+	# 		filters={"role": "Program Manager P&D"},
+	# 		fields=["parent"]
+	# 	)
+
+	# 	for user in users:
+	# 		user_id = user.parent
+
+	# 		notification = frappe.new_doc("Notification Log")
+	# 		notification.update({
+	# 			"subject": "New Project Approval Submitted",
+	# 			"email_content": f"A Project Approval Form has been submitted: {self.name}",
+	# 			"for_user": user_id,
+	# 			"document_type": self.doctype,
+	# 			"document_name": self.name,
+	# 			"type": "Alert"
+	# 		})
+	# 		notification.insert(ignore_permissions=True)
+	# 		frappe.msgprint(f'Notification sent to {user.parent}')
+
+
+
